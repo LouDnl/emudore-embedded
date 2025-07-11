e001d03c9f064611662be0c991a7f7b94ce3a134
loudnl <LouDnl@users.noreply.github.com>
2025-07-11 18:18:06 +0200

    Start with arguments and player fixes

--- src/loader.cpp
+++ src/loader.cpp
@@ -92,14 +92,16 @@ void Loader::load_prg()
     /* basic-tokenized prg */
     if(addr == kBasicPrgStart)
     {
+      if(autorun) {
       /* make BASIC happy */
-      mem_->write_word_no_io(kBasicTxtTab,kBasicPrgStart);
-      mem_->write_word_no_io(kBasicVarTab,pbuf);
-      mem_->write_word_no_io(kBasicAryTab,pbuf);
-      mem_->write_word_no_io(kBasicStrEnd,pbuf);
-      /* exec RUN */
-      for(char &c: std::string("RUN\n"))
-        io_->IO::type_character(c);
+        mem_->write_word_no_io(kBasicTxtTab,kBasicPrgStart);
+        mem_->write_word_no_io(kBasicVarTab,pbuf);
+        mem_->write_word_no_io(kBasicAryTab,pbuf);
+        mem_->write_word_no_io(kBasicStrEnd,pbuf);
+        /* exec RUN */
+          for(char &c: std::string("RUN\n"))
+            io_->IO::type_character(c);
+      }
     }
     /* ML */
     else cpu_->pc(addr);
@@ -114,89 +116,128 @@ void Loader::sid(const std::string &f)
   sidfile_ = new SidFile();
   sidfile_->Parse(f);
 }
-
+extern Loader *loader;
 void Loader::load_sid()
 {
+  pl_songs = sidfile_->GetNumOfSongs();
+  pl_song_number = sidfile_->GetFirstSong(); /* TODO: FIX N FINISH ;) */
+  pl_sidflags = sidfile_->GetSidFlags();
+  pl_sidspeed = sidfile_->GetSongSpeed(pl_song_number); // + 1);
+  pl_curr_sidspeed = pl_sidspeed & (1 << pl_song_number); // ? 1 : 0;  // 1 ~ 60Hz, 2 ~ 50Hz
+  pl_chiptype = sidfile_->GetChipType(1);
+  pl_clockspeed = sidfile_->GetClockSpeed();
+  pl_sidversion = sidfile_->GetSidVersion();
+  // 2 2 3 ~ Genius
+  // 2 1 3 ~ Jump
+  // printf("%d %d %d\n", ct, cs, sv);
+  pl_clock_speed = clockSpeed[pl_clockspeed];
+  pl_raster_lines = scanLines[pl_clockspeed];
+  pl_rasterrow_cycles = scanlinesCycles[pl_clockspeed];
+  pl_frame_cycles = pl_raster_lines * pl_rasterrow_cycles;
+  pl_refresh_rate = refreshRate[pl_clockspeed];
+  pl_loadaddr = sidfile_->GetLoadAddress();
+  pl_datalength = sidfile_->GetDataLength();
+  pl_databuffer = sidfile_->GetDataPtr();
+  pl_playaddr = sidfile_->GetPlayAddress();
+  pl_initaddr = sidfile_->GetInitAddress();
+
   print_sid_info();
   mem_->write_byte(0xD020,2); /* Bordor color to red */
   mem_->write_byte(0xD021,0); /* Background color to black */
-  /* Turn off screen by wrapping the border color over the whole screen */
-  // mem_->write_byte(0xD011,(mem_->read_byte(0xD011) & 0b11101111));
-  uint16_t load = sidfile_->GetLoadAddress();
-  uint16_t len = sidfile_->GetDataLength();
-  uint8_t *buffer = sidfile_->GetDataPtr();
 
-  for (unsigned int i = 0; i < len; i++) {
-    mem_->write_byte((load+i),buffer[i]);
+  /* Load SID data into memory */
+  for (unsigned int i = 0; i < pl_datalength; i++) {
+    mem_->write_byte((pl_loadaddr+i),pl_databuffer[i]);
   }
-  uint16_t play = sidfile_->GetPlayAddress();
-  uint16_t init = sidfile_->GetInitAddress();
+  printf("load: $%04X play: $%04X init: $%04X\n",
+    pl_loadaddr, pl_playaddr, pl_initaddr);
 
-  int songno = sidfile_->GetFirstSong();
-  // cpu_->reset();
+  load_sidplayerA(pl_playaddr, pl_initaddr, pl_song_number);
+  // load_sidplayerB(pl_playaddr, pl_initaddr, pl_song_number);
 
+  sid_->sid_flush();
+  sid_->set_playing(true);
+  cpu_->pc(mem_->read_word(Memory::kAddrResetVector));
+}
+
+void Loader::load_sidplayerA(uint16_t play, uint16_t init, int songno)
+{
   // install reset vector for microplayer (0x0000)
   mem_->write_byte(0xFFFD, 0x00);
-  mem_->write_byte(0xFFFC, 0x00);
+  mem_->write_byte(0xFFFC, 0x02);
 
-  // install IRQ vector for play routine launcher (0x0013)
+  // install IRQ vector for play routine launcher (0x000C)
   mem_->write_byte(0xFFFF, 0x00);
-  mem_->write_byte(0xFFFE, 0x13);
+  mem_->write_byte(0xFFFE, 0x0C);
 
-  mem_->write_byte(0x0001, 0x35); // clear kernel and basic rom from ram
+  // clear kernel and basic rom from ram
+  mem_->write_byte(0x0001, 0x35);
 
   // install the micro player, 6502 assembly code
-  mem_->write_byte(0x0000, 0xA9);               // 0xA9 LDA imm load A with the song number
-  mem_->write_byte(0x0001, songno);             // 0xNN #NN song number
+  mem_->write_byte(0x0002, 0xA9);               // 0xA9 LDA imm load A with the song number
+  mem_->write_byte(0x0003, songno);             // 0xNN #NN song number
 
-  mem_->write_byte(0x0002, 0x20);               // 0x20 JSR abs jump sub to INIT routine
-  mem_->write_byte(0x0003, init & 0xFF);        // 0xxxNN address lo
-  mem_->write_byte(0x0004, (init >> 8) & 0xFF); // 0xNNxx address hi
+  mem_->write_byte(0x0004, 0x20);               // 0x20 JSR abs jump sub to INIT routine
+  mem_->write_byte(0x0005, init & 0xFF);        // 0xxxNN address lo
+  mem_->write_byte(0x0006, (init >> 8) & 0xFF); // 0xNNxx address hi
 
-  mem_->write_byte(0x0005, 0x58);               // 0x58 CLI enable interrupt
-  mem_->write_byte(0x0006, 0xEA);               // 0xEA NOP impl
-  mem_->write_byte(0x0007, 0x4C);               // JMP jump to 0x0006
-  mem_->write_byte(0x0008, 0x06);               // 0xxxNN address lo
-  mem_->write_byte(0x0009, 0x00);               // 0xNNxx address hi
+  mem_->write_byte(0x0007, 0x58);               // 0x58 CLI enable interrupt
+  mem_->write_byte(0x0008, 0xEA);               // 0xEA NOP impl
+  mem_->write_byte(0x0009, 0x4C);               // JMP jump to 0x0006
+  mem_->write_byte(0x000A, 0x08);               // 0xxxNN address lo
+  mem_->write_byte(0x000B, 0x00);               // 0xNNxx address hi
 
-  mem_->write_byte(0x0013, 0xEA);               // 0xEA NOP // 0xA9 LDA imm // A = 1
-  mem_->write_byte(0x0014, 0xEA);               // 0xEA NOP // 0x01 #NN
-  mem_->write_byte(0x0015, 0xEA);               // 0xEA NOP // 0x78 SEI disable interrupt
-  mem_->write_byte(0x0016, 0x20);               // 0x20 JSR jump sub to play routine
-  mem_->write_byte(0x0017, play & 0xFF);        // playaddress lo
-  mem_->write_byte(0x0018, (play >> 8) & 0xFF); // playaddress hi
-  mem_->write_byte(0x0019, 0xEA);               // 0xEA NOP // 0x58 CLI enable interrupt
-  mem_->write_byte(0x001A, 0x40);               // 0x40 RTI return from interrupt
-  // vic_->disable(true); /* Disable screen output */    // TODO: FINISH, IS BROKEN NOW
-  sid_->reset_cycles();
-  // cpu_->reset();
-  cpu_->pc(mem_->read_word(Memory::kAddrResetVector));
+  mem_->write_byte(0x000C, 0xEA);               // 0xEA NOP // 0xA9 LDA imm // A = 1
+  mem_->write_byte(0x000D, 0xEA);               // 0xEA NOP // 0x01 #NN
+  mem_->write_byte(0x000E, 0xEA);               // 0xEA NOP // 0x78 SEI disable interrupt
+  mem_->write_byte(0x000F, 0x20);               // 0x20 JSR jump sub to play routine
+  mem_->write_byte(0x0010, play & 0xFF);        // playaddress lo
+  mem_->write_byte(0x0011, (play >> 8) & 0xFF); // playaddress hi
+  mem_->write_byte(0x0012, 0xEA);               // 0xEA NOP // 0x58 CLI enable interrupt
+  mem_->write_byte(0x0013, 0x40);               // 0x40 RTI return from interrupt
+}
 
-  // mem_->write_byte(0xD020,0); /* Bordor color to black */
-  // mem_->write_byte(0xD021,0); /* Background color to black */
-  // mem_->write_byte(0xD011,(mem_->read_byte(0xD011) & 0b11101111)); /* Turn off screen by wrapping it with border */
+void Loader::load_sidplayerB(uint16_t play, uint16_t init, int songno)
+{ /* BUG: Causes turbo play */
+  // install reset vector for microplayer (0x0000)
+  mem_->write_byte(0xFFFD, 0x02);
+  mem_->write_byte(0xFFFC, 0xA7);
 
+  // install IRQ vector for play routine launcher (0x0813)
+  mem_->write_byte(0xFFFF, 0x02);
+  mem_->write_byte(0xFFFE, 0xB1);
+
+  // clear kernel and basic rom from ram
+  mem_->write_byte(0x0001, 0x35);
+
+  // install the micro player, 6502 assembly code
+  mem_->write_byte(0x02A7, 0xA9);               // 0xA9 LDA imm load A with the song number
+  mem_->write_byte(0x02A8, songno);             // 0xNN #NN song number
+
+  mem_->write_byte(0x02A9, 0x20);               // 0x20 JSR abs jump sub to INIT routine
+  mem_->write_byte(0x02AA, init & 0xFF);        // 0xxxNN address lo
+  mem_->write_byte(0x02AB, (init >> 8) & 0xFF); // 0xNNxx address hi
+
+  // infinite loop?
+  mem_->write_byte(0x02AC, 0x58);               // 0x58 CLI enable interrupt
+  mem_->write_byte(0x02AD, 0xEA);               // 0xEA NOP impl
+  mem_->write_byte(0x02AE, 0x4C);               // JMP jump to 0x0006
+  mem_->write_byte(0x02AF, 0x08);               // 0xxxNN address lo
+  mem_->write_byte(0x02B0, 0x00);               // 0xNNxx address hi
+
+  // install the play routine launcher
+  mem_->write_byte(0x02B1, 0xEA);               // 0xEA NOP // 0xA9 LDA imm // A = 1
+  mem_->write_byte(0x02B2, 0xEA);               // 0xEA NOP // 0x01 #NN
+  mem_->write_byte(0x02B3, 0xEA);               // 0xEA NOP // 0x78 SEI disable interrupt
+  mem_->write_byte(0x02B4, 0x20);               // 0x20 JSR jump sub to play routine
+  mem_->write_byte(0x02B5, play & 0xFF);        // playaddress lo
+  mem_->write_byte(0x02B6, (play >> 8) & 0xFF); // playaddress hi
+  mem_->write_byte(0x02B7, 0xEA);               // 0xEA NOP // 0x58 CLI enable interrupt
+  mem_->write_byte(0x02B8, 0x40);               // 0x40 RTI return from interrupt
 }
 
 void Loader::print_sid_info() /* TODO: THIS MUST GO TO C64 SCREEN */
 {
-  int songs = sidfile_->GetNumOfSongs();
-  int song_number = sidfile_->GetFirstSong(); /* TODO: FIX N FINISH ;) */
-  int sidflags = sidfile_->GetSidFlags();
-  uint32_t sidspeed = sidfile_->GetSongSpeed(song_number); // + 1);
-  int curr_sidspeed = sidspeed & (1 << song_number); // ? 1 : 0;  // 1 ~ 60Hz, 2 ~ 50Hz
-  int ct = sidfile_->GetChipType(1);
-  int cs = sidfile_->GetClockSpeed();
-  int sv = sidfile_->GetSidVersion();
-  // 2 2 3 ~ Genius
-  // 2 1 3 ~ Jump
-  // printf("%d %d %d\n", ct, cs, sv);
-  int clock_speed = clockSpeed[cs];
-  int raster_lines = scanLines[cs];
-  int rasterrow_cycles = scanlinesCycles[cs];
-  int frame_cycles = raster_lines * rasterrow_cycles;
-  int refresh_rate = refreshRate[cs];
-
   cout << "\n< Sid Info >" << endl;
   cout << "---------------------------------------------" << endl;
   cout << "SID Title          : " << sidfile_->GetModuleName() << endl;
@@ -204,26 +245,26 @@ void Loader::print_sid_info() /* TODO: THIS MUST GO TO C64 SCREEN */
   cout << "Release & (C)      : " << sidfile_->GetCopyrightInfo() << endl;
   cout << "---------------------------------------------" << endl;
   cout << "SID Type           : " << sidfile_->GetSidType() << endl;
-  cout << "SID Format version : " << dec << sv << endl;
+  cout << "SID Format version : " << dec << pl_sidversion << endl;
   cout << "---------------------------------------------" << endl;
   // cout << "SID Flags          : 0x" << hex << sidflags << " 0b" << bitset<8>{sidflags} << endl;
-  cout << "Chip Type          : " << chiptype_s[ct] << endl;
-  if (sv == 3 || sv == 4)
+  cout << "Chip Type          : " << chiptype_s[pl_chiptype] << endl;
+  if (pl_sidversion == 3 || pl_sidversion == 4)
       cout << "Chip Type 2        : " << chiptype_s[sidfile_->GetChipType(2)] << endl;
-  if (sv == 4)
+  if (pl_sidversion == 4)
       cout << "Chip Type 3        : " << chiptype_s[sidfile_->GetChipType(3)] << endl;
-  cout << "Clock Type         : " << hex << clockSpeed[cs] << endl;
-  cout << "Clock Speed        : " << dec << clock_speed << endl;
-  cout << "Raster Lines       : " << dec << raster_lines << endl;
-  cout << "Rasterrow Cycles   : " << dec << rasterrow_cycles << endl;
-  cout << "Frame Cycles       : " << dec << frame_cycles << endl;
-  cout << "Refresh Rate       : " << dec << refresh_rate << endl;
-  if (sv == 3 || sv == 4 || sv == 78) {
+  cout << "Clock Type         : " << hex << clockspeed_s[pl_clockspeed] << endl;
+  cout << "Clock Speed        : " << dec << pl_clock_speed << endl;
+  cout << "Raster Lines       : " << dec << pl_raster_lines << endl;
+  cout << "Rasterrow Cycles   : " << dec << pl_rasterrow_cycles << endl;
+  cout << "Frame Cycles       : " << dec << pl_frame_cycles << endl;
+  cout << "Refresh Rate       : " << dec << pl_refresh_rate << endl;
+  if (pl_sidversion == 3 || pl_sidversion == 4 || pl_sidversion == 78) {
       cout << "---------------------------------------------" << endl;
       cout << "SID 2 $addr        : $d" << hex << sidfile_->GetSIDaddr(2) << "0" << endl;
-      if (sv == 4 || sv == 78)
+      if (pl_sidversion == 4 || pl_sidversion == 78)
           cout << "SID 3 $addr        : $d" << hex << sidfile_->GetSIDaddr(3) << "0" << endl;
-      if (sv == 78)
+      if (pl_sidversion == 78)
           cout << "SID 4 $addr        : $d" << hex << sidfile_->GetSIDaddr(4) << "0" << endl;
   }
   cout << "---------------------------------------------" << endl;
@@ -233,16 +274,30 @@ void Loader::print_sid_info() /* TODO: THIS MUST GO TO C64 SCREEN */
   cout << "Init Address       : $" << hex << sidfile_->GetInitAddress() << endl;
   cout << "Play Address       : $" << hex << sidfile_->GetPlayAddress() << endl;
   cout << "---------------------------------------------" << endl;
-  cout << "Song Speed(s)      : $" << hex << curr_sidspeed << " $0x" << hex << sidspeed << " 0b" << bitset<32>{sidspeed} << endl;
-  cout << "Timer              : " << (curr_sidspeed == 1 ? "CIA1" : "Clock") << endl;
-  cout << "Selected Sub-Song  : " << dec << song_number + 1 << " / " << dec << sidfile_->GetNumOfSongs() << endl;
+  cout << "Song Speed(s)      : $" << hex << pl_curr_sidspeed << " $0x" << hex << pl_sidspeed << " 0b" << bitset<32>{pl_sidspeed} << endl;
+  cout << "Timer              : " << (pl_curr_sidspeed == 1 ? "CIA1" : "Clock") << endl;
+  cout << "Selected Sub-Song  : " << dec << pl_song_number + 1 << " / " << dec << sidfile_->GetNumOfSongs() << endl;
 }
 
 
 // emulate //////////////////////////////////////////////////////////////////
 
+void Loader::handle_args()
+{
+  if(start_argc>2) {
+    for(int a = 2; a < start_argc; a++) {
+      if(!strcmp(start_argv[a], "-norun")) autorun = false;
+      if(!strcmp(start_argv[a], "-lowercase")) lowercase = true;
+    }
+  }
+  if (lowercase) {
+    mem_->write_byte(0xD018,0x17); /* Enable lowercase mode */
+  }
+}
+
 bool Loader::emulate()
 {
+  handle_args();
   if(booted_up_)
   {
     switch(format_)
--- src/loader.h
+++ src/loader.h
@@ -46,16 +46,46 @@ class Loader
       kSID
     };
     kFormat format_;
+
+    uint16_t pl_loadaddr;
+    uint16_t pl_initaddr;
+    uint16_t pl_playaddr;
+    uint16_t pl_datalength;
+    uint8_t *pl_databuffer;
+    uint32_t pl_sidspeed;
+    int pl_songs;
+    int pl_song_number;
+    int pl_sidflags;
+    int pl_curr_sidspeed;
+    int pl_chiptype;
+    int pl_clockspeed;
+    int pl_sidversion;
+
+    int pl_clock_speed;
+    int pl_raster_lines;
+    int pl_rasterrow_cycles;
+    int pl_frame_cycles;
+    int pl_refresh_rate;
+
+    int start_argc;
+    char **start_argv;
+    bool autorun = true;
+    bool lowercase = false;
+
     void load_basic();
     void load_prg();
     void load_sid();
+    void load_sidplayerA(uint16_t play, uint16_t init, int songno);
+    void load_sidplayerB(uint16_t play, uint16_t init, int songno);
     void print_sid_info(); /* TODO: Print on C64 screen */
+    void handle_args();
     uint16_t read_short_le();
   public:
     Loader(C64 *c64);
     void bas(const std::string &f);
     void prg(const std::string &f);
     void sid(const std::string &f);
+    void set_args(int argc, char **argv) {start_argc=argc;start_argv=argv;};
     bool emulate();
     /* constants */
     static const uint16_t kBasicPrgStart = 0x0801;
