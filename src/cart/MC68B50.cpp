a7bb3edf336735a567db87acc6615f49e4158a80
loudnl <LouDnl@users.noreply.github.com>
2025-08-23 15:36:59 +0200

    Tidy up MC6850 object

--- src/cart/MC68B50.cpp
+++ src/cart/MC68B50.cpp
@@ -1,87 +1,56 @@
+/*
+ * Motorola 68B50 ACIA emulation
+ * (for Midi and or Uart communication)
+ * Copyright (c) 2025, LouD <emudore@mail.loudai.nl>
+ *
+ * MC68B50.cpp
+ *
+ * Made for emudore, Commodore 64 emulator
+ * Copyright (c) 2016, Mario Ballano <mballano@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <c64.h>
 #include <MC68B50.h>
 #include <util.h>
 
 
-enum ACIARegisters
+/**
+ * @brief Constructor setup must happen _after_ memory
+ * @param C64 * c64 a pointer to the main object
+ * @return MC68B50 object
+ */
+MC68B50::MC68B50(C64 * c64)
+  : c64_(c64)
 {
-  CONTROL = 0x04, /* control register       ~ write only */
-  STATUS  = 0x06, /* status register        ~ read only */
-  TXDR    = 0x05, /* transmit data register ~ write only */
-  RXDR    = 0x07, /* receive data register  ~ read  only */
-};
-
-enum ControlBitMasks
-{
-  CR0CR1SEL = 0b00000011, /* Counter divide select bits */
-  WORDSEL   = 0b00011100, /* Word select bits */
-  TCCTR     = 0b01100000, /* Transmit control bits */
-  INTEN     = 0b10000000, /* Receive interrupt enable bit */
-};
-
-enum CounterDivideBits
-{
-  R1  = 0b00, /* set divide ratio +1 */
-  R16 = 0b01, /* set divide ratio +16 */
-  R64 = 0b10, /* set divide ratio +64 */
-  RES = 0b11, /* reset midi device */
-};
-
-enum WordSelectBits
-{
-  w7e2 = 0b000, /* 7 Bits + Even Parity + 2 Stop Bits */
-  w7o2 = 0b001, /* 7 Bits + Odd Parity + 2 Stop Bits */
-  w7e1 = 0b010, /* 7 Bits + Even Parity + 1 Stop Bit */
-  w7o1 = 0b011, /* 7 Bits + Odd Parity + 1 Stop Bit */
-  w8n2 = 0b100, /* 8 Bits + 2 Stop Bits */
-  w8n1 = 0b101, /* 8 Bits + 1 Stop Bit */
-  w8e1 = 0b110, /* 8 Bits + Even Parity + 1 Stop Bit */
-  w8o1 = 0b111, /* 8 Bits + Odd Parity + 1 Stop Bit */
-};
-
-enum TransmitControlBits
-{
-  RTSloTID = 0b00, /* RTS=low, Transmitting Interrupt Disabled. */
-  RTSloTIE = 0b01, /* RTS=low, Transmitting Interrupt Enabled. */
-  RTShiTID = 0b10, /* RTS=high, Transmitting Interrupt Disabled */
-  RTSloTRB = 0b11, /* RTS=low, Transmits a Break level on the Transmit Data Output. Transmitting Interrupt Disabled. */
-};
-
-enum StatusRegister
-{
-  RDRF = (1<<0), /* Receive Data Register Full */
-  TDRE = (1<<1), /* Transmit Data Register Empty */
-  DCD  = (1<<2), /* Data Carrier Detect */
-  CTS  = (1<<3), /* Clear-to-Send */
-  FE   = (1<<4), /* Framing Error */
-  RO   = (1<<5), /* Receiver Overrun */
-  PE   = (1<<6), /* Parity Error */
-  IRQ  = (1<<7), /* Interrupt Request */
-};
-
+  /* TODO: Need to set TDRE to STATUS here
+   * but after memory pointer initalization */
+}
 
 /**
- * @brief: Setup must happen _after_ memory
+ * @brief resets the MC6850 STATUS register
  */
-MC68B50::MC68B50(C64 * c64) :
-  c64_(c64)
-{
-  // c64_->cart_->init_mc6850();
-}
-
-MC68B50::~MC68B50()
-{
-
-}
-
 void MC68B50::reset()
 {
+  /* ISSUE: Calling this from anywhere else then
+   * from `write_register` seems to break rules
+   * and causes a segmentation fault */
   c64_->cart_->k6850MemRd[STATUS] = TDRE; /* Set TDRE default to empty */
 }
 
 /**
  * @brief Handle Midi keyboard key down event
- *
  */
 void MC68B50::fake_keydown(void)
 {
@@ -106,7 +75,6 @@ void MC68B50::fake_keydown(void)
 
 /**
  * @brief Handle Midi keyboard key up event
- *
  */
 void MC68B50::fake_keyup(void)
 {
@@ -125,24 +93,28 @@ void MC68B50::fake_keyup(void)
 }
 
 /**
- * @brief reads a byte from a Motorola MC68B50 ACIA register @ $denn
+ * @brief reads a byte from MC6850 readable registers
+ * @param uint8_t r the register to read
+ * @return uint8_t the registers value
+ * configured for Datel/Kerberos
+ * Registers for other brands can be at different
+ * addresses depending on the type of Cart
  */
 uint8_t MC68B50::read_register(uint8_t r)
 {
   uint8_t retval = 0;
   switch (r) {
     case CONTROL: /* $de04 ~ control register ~ write only */
-      retval = c64_->cart_->k6850MemRd[r]; // Return Wr register?
       break;
     case STATUS:  /* $de06 ~ status register  ~ read only */
       retval = c64_->cart_->k6850MemRd[r];
       break;
     case TXDR:    /* $de05 ~ TX register      ~ write only */
-      break;
+      break;;
     /* NOTE: Data in the RXDR should come from a ringbuffer that shifts after reading */
     case RXDR:    /* $de07 ~ RX register      ~ read only */
       c64_->cart_->k6850MemRd[STATUS] &= ~(IRQ|RDRF); /* Clear IRQ and RDRF on read (if set) */
-      if (n_read < 3) {
+      if (n_read < 3) { /* NOTE: This is fake to emulate Midi input */
         if (_keydown) c64_->cart_->k6850MemRd[RXDR] = midi_keydown[n_read++];
         if (_keyup) c64_->cart_->k6850MemRd[RXDR] = midi_keyup[n_read++];
         c64_->cart_->k6850MemRd[STATUS] |= (IRQ|RDRF); /* Set IRQ and RDRF for data available */
@@ -153,7 +125,7 @@ uint8_t MC68B50::read_register(uint8_t r)
         c64_->cart_->k6850MemRd[STATUS] &= ~(IRQ|RDRF); /* Clear IRQ and RDRF on read (if set) */
       }
       break;
-    default:
+    default:      /* default always return read */
       retval = c64_->cart_->k6850MemRd[r];
       break;
   }
@@ -161,13 +133,22 @@ uint8_t MC68B50::read_register(uint8_t r)
   return retval;
 }
 
+/**
+ * @brief Write a byte to MC6850 writeable registers
+ * @param uint8_t r the register to write to
+ * @param uint8_t v the value to write
+ * configured for Datel/Kerberos
+ * Registers for other brands can be at different
+ * addresses depending on the type of Cart
+ */
 void MC68B50::write_register(uint8_t r, uint8_t v)
 {
   uint8_t cr = 0; /* control register masked value */
   switch (r) {
     case CONTROL: /* $de04 ~ control register ~ write only */
-      c64_->cart_->k6850MemWr[r] = c64_->cart_->k6850MemRd[r] = v; /* Save value to ram */
-      cr = (v & CR0CR1SEL);      /* Counter divide select bits */
+      /* Save value to write ram */
+      c64_->cart_->k6850MemWr[r] = v;
+      cr = (v & CR0CR1SEL); /* Counter divide select bits */
       switch (cr) {
         case R1: /* set divide ratio +1 */
           D("[MC68B50] divide ratio +1\n");
@@ -180,63 +161,89 @@ void MC68B50::write_register(uint8_t r, uint8_t v)
           break;
         case RES: /* reset midi device */
           D("[MC68B50] Master reset!\n");
-          // c64_->cart_->k6850MemRd[STATUS] = TDRE;
-          reset();
+          this->reset();
           break;
         default: break;
       }
+      if (cr == RES) {return;}; /* Return here if Master reset */
       cr = ((v & WORDSEL) >> 2); /* Word select bits */
       switch (cr) {
-        case w7e2: break; /* Unused for CynthCart */
-        case w7o2: break; /* Unused for CynthCart */
-        case w7e1: break; /* Unused for CynthCart */
-        case w7o1: break; /* Unused for CynthCart */
-        case w8n2: break; /* Unused for CynthCart */
+        case w7e2:
+          D("[MC68B50] 7 Bits + Even Parity + 2 Stop Bits\n");
+          break; /* Unused for CynthCart */
+        case w7o2:
+          D("[MC68B50] 7 Bits + Odd Parity + 2 Stop Bits\n");
+          break; /* Unused for CynthCart */
+        case w7e1:
+          D("[MC68B50] 7 Bits + Even Parity + 1 Stop Bit\n");
+          break; /* Unused for CynthCart */
+        case w7o1:
+          D("[MC68B50] 7 Bits + Odd Parity + 1 Stop Bit\n");
+          break; /* Unused for CynthCart */
+        case w8n2:
+          D("[MC68B50] 8 Bits + 2 Stop Bits\n");
+          break; /* Unused for CynthCart */
         case w8n1: /* 8 Bits+1 Stop Bit */
           D("[MC68B50] 8 Bits+1 Stop Bit\n");
           break;
-        case w8e1: break; /* Unused for CynthCart */
-        case w8o1: break; /* Unused for CynthCart */
+        case w8e1:
+          D("[MC68B50] 8 Bits + Even Parity + 1 Stop Bit\n");
+          break; /* Unused for CynthCart */
+        case w8o1:
+          D("[MC68B50] 8 Bits + Odd Parity + 1 Stop Bit\n");
+          break; /* Unused for CynthCart */
         default: break;
       }
       cr = ((v & TCCTR) >> 5);   /* Transmit control bits */
       switch (cr) {
-        case RTSloTID:        /* RTS=low, Transmitting Interrupt Disabled. */
+        case RTSloTID: /* RTS=low, Transmitting Interrupt Disabled. */
           D("[MC68B50] RTS=low, Transmitting Interrupt Disabled\n");
           break;
-        case RTSloTIE: break; /* RTS=low, Transmitting Interrupt Enabled. */
-        case RTShiTID: break; /* RTS=high, Transmitting Interrupt Disabled */
-        case RTSloTRB: break; /* RTS=low, Transmits a Break level on the Transmit Data Output. Transmitting Interrupt Disabled. */
+        case RTSloTIE: /* RTS=low, Transmitting Interrupt Enabled. */
+          D("[MC68B50] RTS=low, Transmitting Interrupt Enabled\n");
+          break;
+        case RTShiTID: /* RTS=high, Transmitting Interrupt Disabled */
+          D("[MC68B50] RTS=high, Transmitting Interrupt Disabled\n");
+          break;
+        case RTSloTRB: /* RTS=low, Transmits a Break level on the Transmit Data Output. Transmitting Interrupt Disabled. */
+          D("[MC68B50] RTS=low, Transmits a Break level on the Transmit Data Output. Transmitting Interrupt Disabled\n");
+          break;
         default: break;
       }
       cr = ((v & INTEN) >> 7);   /* Receive interrupt enable bit */
       switch (cr) {
-        case 0b0: break; /* Receive interrupt disabled */
+        case 0b0: /* Receive interrupt disabled */
+          D("[MC68B50] Receive interrupt disabled\n");
+          break;
         case 0b1: /* Receive interrupt enabled */
           D("[MC68B50] Receive interrupt enabled\n");
           break;
         default: break;
       }
-      break;
+      return;
     case STATUS:  /* $de06 ~ status register  ~ read only */
-      // c64_->cart_->k6850MemWr[r] = c64_->cart_->k6850MemRd[r] = v; /* Save value to ram */
-      break;
-    case TXDR:    /* $de05 ~ TX register      ~ write only*/
-      c64_->cart_->k6850MemWr[r] = c64_->cart_->k6850MemRd[r] = v; /* Save value to ram */
-      break;
+      return;
+    case TXDR:    /* $de05 ~ TX register      ~ write only */
+      c64_->cart_->k6850MemWr[r] = v; /* Save value to ram */
+      return;
     case RXDR:    /* $de07 ~ RX register      ~ read only */
-      break;
+      return;
     default:      /* default always write to read and write */
       c64_->cart_->k6850MemWr[r] = c64_->cart_->k6850MemRd[r] = v;
-      break;
+      return;
   }
 }
 
+/**
+ * @brief emulate triggers a CPU IRQ if no CPU IRW
+ * is presently active
+ * Other brands then Datel/Kerberos might require
+ * a NMI to trigger, this is not supported yet
+ */
 void MC68B50::emulate()
 {
   if(!c64_->cpu_->idf()) { /* If no CPU IRQ already in place */
     if (c64_->cart_->k6850MemRd[STATUS] & IRQ) { /* If IRQ is set */
-      D("[MC68B50] Trigger CPU IRQ!\n");
       c64_->cpu_->irq(); /* Trigger CPU IRQ */
     }
   }
--- src/cart/MC68B50.h
+++ src/cart/MC68B50.h
@@ -1,9 +1,32 @@
+/*
+ * Motorola 68B50 ACIA emulation
+ * (for Midi and or Uart communication)
+ * Copyright (c) 2025, LouD <emudore@mail.loudai.nl>
+ *
+ * MC68B50.h
+ *
+ * Made for emudore, Commodore 64 emulator
+ * Copyright (c) 2016, Mario Ballano <mballano@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef EMUDORE_65B80_H
 #define EMUDORE_65B80_H
 
-
 #include <iostream>
 #include <cstdint>
+
 
 /* Fake data for testing */
 /* NoteOn/Channel1, Note, Touchspeed */
@@ -14,12 +37,106 @@ static bool _keydown = false, _keyup = true;
 static int n_read = 3;
 
 /**
+ * @brief Parent class containing register addresses,
+ * bitmasks and bits
+ */
+class MC6850BitMasks
+{
+  protected:
+
+    enum ACIARegisters
+    { /* DATEL/SIEL/JMS/C-LAB (Kerberos) */
+      CONTROL  = 0x04, /* control register       ~ write only */
+      STATUS   = 0x06, /* status register        ~ read only */
+      TXDR     = 0x05, /* transmit data register ~ write only */
+      RXDR     = 0x07, /* receive data register  ~ read  only */
+    };
+
+    enum ControlBitMasks
+    {
+      CR0CR1SEL = 0b00000011, /* Counter divide select bits */
+      WORDSEL   = 0b00011100, /* Word select bits */
+      TCCTR     = 0b01100000, /* Transmit control bits */
+      INTEN     = 0b10000000, /* Receive interrupt enable bit */
+    };
+
+    enum CounterDivideBits
+    {
+      R1  = 0b00, /* set divide ratio +1 */
+      R16 = 0b01, /* set divide ratio +16 */
+      R64 = 0b10, /* set divide ratio +64 */
+      RES = 0b11, /* reset midi device */
+    };
+
+    enum WordSelectBits
+    {
+      w7e2 = 0b000, /* 7 Bits + Even Parity + 2 Stop Bits */
+      w7o2 = 0b001, /* 7 Bits + Odd Parity + 2 Stop Bits */
+      w7e1 = 0b010, /* 7 Bits + Even Parity + 1 Stop Bit */
+      w7o1 = 0b011, /* 7 Bits + Odd Parity + 1 Stop Bit */
+      w8n2 = 0b100, /* 8 Bits + 2 Stop Bits */
+      w8n1 = 0b101, /* 8 Bits + 1 Stop Bit */
+      w8e1 = 0b110, /* 8 Bits + Even Parity + 1 Stop Bit */
+      w8o1 = 0b111, /* 8 Bits + Odd Parity + 1 Stop Bit */
+    };
+
+    enum TransmitControlBits
+    {
+      RTSloTID = 0b00, /* RTS=low, Transmitting Interrupt Disabled. */
+      RTSloTIE = 0b01, /* RTS=low, Transmitting Interrupt Enabled. */
+      RTShiTID = 0b10, /* RTS=high, Transmitting Interrupt Disabled */
+      RTSloTRB = 0b11, /* RTS=low, Transmits a Break level on the Transmit Data Output. Transmitting Interrupt Disabled. */
+    };
+
+    enum StatusRegisterBits
+    {
+      RDRF = (1<<0), /* Receive Data Register Full */
+      TDRE = (1<<1), /* Transmit Data Register Empty */
+      DCD  = (1<<2), /* Data Carrier Detect */
+      CTS  = (1<<3), /* Clear-to-Send */
+      FE   = (1<<4), /* Framing Error */
+      RO   = (1<<5), /* Receiver Overrun */
+      PE   = (1<<6), /* Parity Error */
+      IRQ  = (1<<7), /* Interrupt Request */
+    };
+
+    /* Interface registers per Midi device type */
+    enum ACIARegisters_all_unused
+    {
+      /* SEQUENTIAL CIRCUITS INC. */
+      CONTROL_1  = 0x00, /* control register       ~ write only */
+      STATUS_1   = 0x02, /* status register        ~ read only */
+      TXDR_1     = 0x01, /* transmit data register ~ write only */
+      RXDR_1     = 0x03, /* receive data register  ~ read  only */
+      /* PASSPORT & SENTECH */
+      CONTROL_2  = 0x08, /* control register       ~ write only */
+      STATUS_2   = 0x08, /* status register        ~ read only */
+      TXDR_2     = 0x09, /* transmit data register ~ write only */
+      RXDR_2     = 0x09, /* receive data register  ~ read  only */
+      /* DATEL/SIEL/JMS/C-LAB (Kerberos) */
+      CONTROL_3  = 0x04, /* control register       ~ write only */
+      STATUS_3   = 0x06, /* status register        ~ read only */
+      TXDR_3     = 0x05, /* transmit data register ~ write only */
+      RXDR_3     = 0x07, /* receive data register  ~ read  only */
+      /* NAMESOFT ~ SAME REGISTERS AS SEQUENTIAL SO NOT USED! */
+      CONTROL_4  = 0x00, /* control register       ~ write only */
+      STATUS_4   = 0x02, /* status register        ~ read only */
+      TXDR_4     = 0x01, /* transmit data register ~ write only */
+      RXDR_4     = 0x03, /* receive data register  ~ read  only */
+    };
+
+  public:
+    MC6850BitMasks(){};
+    ~MC6850BitMasks(){};
+};
+
+/**
  * @brief Motorola 68B50 Asynchronous Communications Interface Adapter
  *
  * Emulates the Motorola 68B50 ACIA (for Midi and or Uart communication)
  *
  */
-class MC68B50
+class MC68B50 : protected MC6850BitMasks
 {
   private:
 
@@ -28,7 +145,7 @@ class MC68B50
 
   public:
     MC68B50(C64 * c64);
-    ~MC68B50();
+    ~MC68B50(){};
 
     void write_register(uint8_t r, uint8_t v);
     uint8_t read_register(uint8_t r);
