71fc60348b31a4c39f6ba2ffc78813b15e94efd7
loudnl <LouDnl@users.noreply.github.com>
2025-08-23 15:45:36 +0200

    Add alpha version of CRT file loading

--- src/cart.cpp
+++ src/cart.cpp
@@ -20,8 +20,13 @@
  * limitations under the License.
  */
 
+#include <fstream>
+
 #include <c64.h>
 #include <MC68B50.h>
+
+char *cartfile;
+std::ifstream is_;
 
 
 /**
@@ -30,22 +35,27 @@
 Cart::Cart(C64 * c64) :
   c64_(c64)
 {
-  havecart = c64_->havecart;
+  cartactive = c64_->havecart;
+  midi = c64_->midi;
 
-  if (havecart) { /* TODO: Define cart type from .crt file if applied */
-    cartactive = true;
-    mc6850_ = new MC68B50(c64_); /* TODO: Only if needed for cart type */
+  if (midi && !acia_active) {
     init_mc6850();
+    mc6850_ = new MC68B50(c64_);
+    // mc6850_->reset();
+  }
+
+  if (cartactive) {
+    load_crt(c64_->cartfile);
   }
 }
 
 Cart::~Cart()
 {
-  if (cartactive)  { delete mc6850_; }
   if (acia_active) { delete [] mem_rom_mc6850_; }
+  if (midi) { delete mc6850_; }
 }
 
-void Cart::init_mc6850(void)
+void Cart::init_mc6850(void) /* TOOD: Move to MC68B50.cpp */
 {
   mem_rom_mc6850_ = new uint8_t[Memory::kPageSize]();
   acia_active = true;
@@ -53,23 +63,30 @@ void Cart::init_mc6850(void)
   k6850MemRd = &mem_rom_mc6850_[0];
 }
 
+void Cart::deinit_cart(void)
+{ /* RUN/STOP+RESTORE resets cart contents! */
+  if (acia_active) { delete [] mem_rom_mc6850_; } /* TOOD: Move to MC68B50.cpp */
+  if (midi) { delete mc6850_; } /* TOOD: Move to MC68B50.cpp */
+  acia_active = false;
+  midi = false;
+  cartactive = false;
+}
+
 void Cart::reset(void)
 {
-  if (cartactive) {
-    mc6850_->reset();
-  }
+  deinit_cart();
 }
 
 void Cart::emulate(void)
 {
-  if (cartactive) {
+  if (midi) {
     mc6850_->emulate();
   }
 }
 
 void Cart::write_register(uint8_t r, uint8_t v)
 {
-  if (cartactive) {
+  if (midi) {
     mc6850_->write_register(r,v);
   }
 }
@@ -77,8 +94,202 @@ void Cart::write_register(uint8_t r, uint8_t v)
 uint8_t Cart::read_register(uint8_t r)
 {
   uint8_t retval = 0;
-  if (cartactive) {
+  if (midi) {
     retval = mc6850_->read_register(r);
   }
   return retval;
+}
+
+
+// CRT //////////////////////////////////////////////////////////////////////
+
+uint16_t Cart::read_short_le()
+{
+  char b;
+  uint16_t v = 0;
+  is_.get(b);
+  v |= (b);
+  is_.get(b);
+  v |= b << 8;
+  return v;
+}
+
+uint16_t Cart::read_short_be()
+{
+  char b;
+  uint16_t v = 0;
+  is_.get(b);
+  v |= (b << 8);
+  is_.get(b);
+  v |= (b);
+  return v;
+}
+
+void Cart::load_crt(const std::string &f)
+{
+  is_.open(f,std::ios::in|std::ios::binary);
+  Cart::crt_header_t cart_header;
+  Cart::crt_chip_t   chip_header;
+  char b;
+  uint16_t cbuf;
+  if (is_.is_open()) {
+    is_.seekg(0, is_.end);
+    std::streamoff length = is_.tellg();
+    is_.seekg(0, is_.beg);
+    D("CART SIZE: %u\n",length);
+    uint8_t * cart_sig = new uint8_t[16]();
+    is_.read((char *) &cart_sig[0],cart_header.signature_l);
+    int n = memcmp(cart_sig, cart_header.signature_id_s, 16);
+    D("Equal (0)? %d\n",n);
+    delete [] cart_sig;
+    is_.seekg(cart_header.version);
+    uint16_t v = read_short_be();
+    std::streamoff pos = is_.tellg();
+    D("VERSION? %d POS: 0x%04X\n",v,pos);
+    is_.seekg(cart_header.hardware_type);
+    uint16_t h = read_short_be();
+    pos = is_.tellg();
+    D("HARDWARE TYPE? %d IN SPEC? %d POS: 0x%04X\n",h,(h<=27),pos);
+    is_.get(b);
+    uint8_t e = b;
+    is_.get(b);
+    uint8_t g = b;
+    D("EXROM? %d GAME %d\n",e,g);
+    // is_.seekg(0, is_.beg);
+    /* Count number of Chip ROMS */
+    int n_chips = 0;
+    size_t offset = 0;
+    is_.seekg(cart_header.cart_data);
+    uint8_t * chip_sig = new uint8_t[4]();
+CHIP:;
+    is_.read((char *) &chip_sig[0],chip_header.signature_l);
+    int c = memcmp(chip_sig, chip_header.signature_id_s, 4);
+    pos = is_.tellg();
+    is_.seekg(pos-4);
+    pos = is_.tellg();
+    D("POS IS: 0x%04X C IS: %d\n",pos,c);
+NOCHIP:;
+    if(c==0) { /* Chips = 1 */
+      n_chips++;
+      D("Chip (0)? %d No. chips: %d\n",c,n_chips);
+      is_.seekg(cart_header.cart_data+(0x2010*n_chips));
+      // is_.seekg(cart_header.cart_data+(n_chips*0x10));
+      pos = is_.tellg();
+      if (pos < length) {
+        goto CHIP;
+      } else {
+        c = -1;
+        goto NOCHIP;
+      }
+    } else {
+      delete [] chip_sig;
+      D("No. chips found: %d\n",n_chips);
+      D("ROMS start at $%04X\n",is_.tellg());
+    }
+    /* Get chip data */
+    is_.seekg(cart_header.cart_data);
+    // cart_chip_t chips[n_chips]; /* Must be public to switch bank into */
+    chips = new Cart::cart_chip_t[n_chips](); /* Must be public to switch bank into */
+    for (size_t n = 0; n < n_chips; n++) {
+      D("CHIP%d\n",n+1);
+      is_.get(b);
+      chips[n].signature |= (b<<24);
+      is_.get(b);
+      chips[n].signature |= (b<<16);
+      is_.get(b);
+      chips[n].signature |= (b<<8);
+      is_.get(b);
+      chips[n].signature |= (b);
+      D("SIGNATURE: 0x%X\n",chips[n].signature);
+      is_.get(b);
+      chips[n].packetlength |= (b<<24);
+      is_.get(b);
+      chips[n].packetlength |= (b<<16);
+      is_.get(b);
+      chips[n].packetlength |= (b<<8);
+      is_.get(b);
+      chips[n].packetlength |= (b);
+      D("PACKETLENGTH: 0x%X %u\n",chips[n].packetlength,chips[n].packetlength);
+      is_.get(b);
+      chips[n].chiptype |= (b<<8);
+      is_.get(b);
+      chips[n].chiptype |= (b);
+      D("CHIPTYPE: 0x%X %u\n",chips[n].chiptype,chips[n].chiptype);
+      is_.get(b);
+      chips[n].banknumber |= (b<<8);
+      is_.get(b);
+      chips[n].banknumber |= (b);
+      D("BANKNUMBER: 0x%X %u\n",chips[n].banknumber,chips[n].banknumber);
+      is_.get(b);
+      chips[n].addr |= (b<<8);
+      is_.get(b);
+      chips[n].addr |= (b);
+      D("START LOAD ADDRESS: 0x%X %u\n",chips[n].addr,chips[n].addr);
+      is_.get(b);
+      chips[n].romsize |= (b<<8);
+      is_.get(b);
+      chips[n].romsize |= (b);
+      D("ROM SIZE 0x%X %u\n",chips[n].romsize,chips[n].romsize);
+      /* Needed!? */
+      chips[n].rom = new uint8_t[chips[n].romsize]();
+      is_.read ((char *) &chips[n].rom[0],chips[n].romsize);
+      // c64_->mem_->kCARTRomLo = &chips[n].rom[0];
+      D("kCARTRomLo @ 0x%x\n",&c64_->mem_->kCARTRomLo);
+    }
+
+    /* Start with default */
+    banksetup = (PLA::kEXROM|PLA::kGAME|PLA::kCHARGEN|PLA::kHIRAM|PLA::kLORAM);
+    /* Set game bit to 0 if linestate byte = 0 */
+    if (g == 0) {banksetup &= ~(PLA::kGAME);};
+    /* Set exrom bit to 0 if linestate byte = 0 */
+    if (e == 0) {banksetup &= ~(PLA::kEXROM);};
+    /* Banksetup will be set from PLA() now */
+
+    // c64_->pla_->setup_memory_banks(banksetup);
+    // uint8_t banksetup_curr = c64_->mem_->read_byte_no_io(Memory::kAddrMemoryLayout);
+    // uint8_t banksetup = (e<<PLA::kCHI|g<<PLA::kCLO|)
+    // c64_->pla_->setup_memory_banks();
+
+    // TODO: Set rom locations based on chips on cart!
+    c64_->mem_->kCARTRomLo  = &chips[0].rom[0];
+    /* c64_->mem_->kCARTRomHi1 = &chips[1].rom[0]; */
+    // c64_->mem_->kCARTRomHi2 = &chips[1].rom[0];
+
+    D("4BYTES ROMLO: $%02X $%02X $%02X $%02X\n",
+      c64_->mem_->kCARTRomLo[0],
+      c64_->mem_->kCARTRomLo[1],
+      c64_->mem_->kCARTRomLo[2],
+      c64_->mem_->kCARTRomLo[3]
+    );
+    // D("4BYTES ROMHI1: $%02X $%02X $%02X $%02X\n",
+    //   c64_->mem_->kCARTRomHi1[0],
+    //   c64_->mem_->kCARTRomHi1[1],
+    //   c64_->mem_->kCARTRomHi1[2],
+    //   c64_->mem_->kCARTRomHi1[3]
+    // );
+    // D("4BYTES ROMHI2: $%02X $%02X $%02X $%02X\n",
+    //   c64_->mem_->kCARTRomHi2[0],
+    //   c64_->mem_->kCARTRomHi2[1],
+    //   c64_->mem_->kCARTRomHi2[2],
+    //   c64_->mem_->kCARTRomHi2[3]
+    // );
+
+    // D("4BYTES: $%02X $%02X $%02X $%02X\n",
+    //   chips[0].rom[0],
+    //   chips[0].rom[1],
+    //   chips[0].rom[2],
+    //   chips[0].rom[3]
+    // );
+
+    /* TODO: Fix jump address per cart */
+    c64_->cpu_->pc(chips[0].addr);
+
+    // D("4BYTES: $%02X $%02X $%02X $%02X\n",
+    //   c64_->memory()->kCARTRomLo[0],
+    //   c64_->memory()->kCARTRomLo[1],
+    //   c64_->memory()->kCARTRomLo[2],
+    //   c64_->memory()->kCARTRomLo[3]
+    // );
+    is_.close();
+  }
 }
--- src/cart.h
+++ src/cart.h
@@ -31,20 +31,24 @@ class MC68B50;
 class Cart
 {
   private:
+    uint16_t read_short_le(void);
+    uint16_t read_short_be(void);
+    void load_crt(const std::string &f);
+
     /* MC68B50 ACIA ROM & RAM */
     uint8_t *mem_rom_mc6850_;
 
     /* Main pointer */
     C64 * c64_;
 
-    /* Cart yes or no? */
-    bool havecart = false;
-    bool cartactive = false;
+    /* Cart uses mc6850 for midi yes or no? */
+    bool midi = false;
 
   public:
     Cart(C64 * c64);
     ~Cart();
 
+    void deinit_cart(void);
     void reset(void);
     void emulate(void);
 
@@ -59,6 +63,101 @@ class Cart
     bool acia_active = false;
     uint8_t *k6850MemWr; /* $de00 */
     uint8_t *k6850MemRd; /* $de00 */
+
+    /* Cart stuff */
+    uint8_t banksetup; /* Used by PLA if havecart == true */
+    bool cartactive = false;
+
+    /* For reference only */
+    enum crt_hardware_type {
+      NORMAL_CARTRIDGE,    // 0
+      ACTION_REPLAY,       // 1
+      KCS_POWER_CARTRIDGE, // 2
+      FINAL_CARTRIDGE_III, // 3
+      SIMONS_BASIC,        // 4
+      OCEAN_TYPE_1,        // 5
+      EXPERT_CARTRIDGE,    // 6
+      FUN_PLAY_POWER_PLAY, // 7
+      SUPER_GAMES,         // 8
+      ATOMIC_POWER,        // 9
+      EPYX_FASTLOAD,       // 10
+      WESTERMANN_LEARNING, // 11
+      REX_UTILITY,         // 12
+      FINAL_CARTRIDGE_I,   // 13
+      MAGIC_FORMEL,        // 14
+      C64_GAME_SYSTEM_3,   // 15
+      WARPSPEED,           // 16
+      DINAMIC,             // 17
+      SUPER_ZAXXON_SEGA,   // 18
+      MAGIC_DESK,          // 19
+      SUPER_SNAPSHOT_5,    // 20
+      COMAL_80,            // 21
+      STRUCTURED_BASIC,    // 22
+      ROSS,                // 23
+      DELA_EP64,           // 24
+      DELA_EP7X8,          // 25
+      DELA_EP256,          // 26
+      REX_EP256,           // 27
+    };
+
+    typedef struct crt_header_s {
+      uint8_t signature_id_s[16] =
+        {0x43,0x36,0x34, /* C64 */
+         0x20, /* space */
+         0x43,0x41,0x52,0x54,0x52,0x49,0x44,0x47,0x45, /* CARTRIDGE */
+         0x20,0x20}; /* space space */
+      uint8_t signature          = 0x00;
+      uint8_t signature_l        = 0x0F;
+      uint8_t headerlength       = 0x10;
+      uint8_t headerlength_l     = 0x04;
+      uint8_t version            = 0x14;
+      uint8_t version_l          = 0x02;
+      uint8_t hardware_type      = 0x16;
+      uint8_t hardware_type_l    = 0x02;
+      uint8_t exrom_line_status  = 0x18;
+      uint8_t game_line_status   = 0x19;
+      uint8_t unused             = 0x1a;
+      uint8_t unused_l           = 0x05;
+      uint8_t cartname           = 0x20;
+      uint8_t cartname_l         = 0x1f;
+      uint8_t cart_data          = 0x40;
+    } crt_header_t;
+
+    /* For reference only */
+    enum crt_chip_type {
+      ROM,      /* 0 */
+      RAMnoROM, /* 1 */
+      FlashROM, /* 2 */
+    };
+
+    typedef struct crt_chip_s {
+      uint8_t signature_id_s[4] =
+        {0x43,0x48,0x49,0x50}; /* CHIP */
+      uint8_t signature         = 0x00;
+      uint8_t signature_l       = 0x04;
+      uint8_t packetlength      = 0x04;
+      uint8_t packetlength_l    = 0x04;
+      uint8_t chiptype          = 0x08;
+      uint8_t chiptype_l        = 0x02;
+      uint8_t banknumber        = 0x0a;
+      uint8_t banknumber_l      = 0x02;
+      uint8_t start_load_addr   = 0x0c;
+      uint8_t start_load_addr_l = 0x02;
+      uint8_t rom_img_size      = 0x0e;
+      uint8_t rom_img_size_l    = 0x02;
+      uint8_t rom_data          = 0x10;
+    } crt_chip_t;
+
+    typedef struct cart_chip_s {
+      uint32_t signature     = 0;
+      uint32_t packetlength  = 0;
+      uint16_t chiptype      = 0;
+      uint16_t banknumber    = 0;
+      uint16_t addr          = 0;
+      uint16_t romsize       = 0;
+      uint8_t  *rom;
+    } cart_chip_t;
+    cart_chip_t * chips;
 
 };
 
