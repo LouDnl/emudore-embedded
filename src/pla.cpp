9dcf3c740944a47d42f2c9c5768917ab7de4f87f
loudnl <LouDnl@users.noreply.github.com>
2025-08-23 15:43:08 +0200

    Rework PLA to support different cart type bank switching

--- src/pla.cpp
+++ src/pla.cpp
@@ -36,8 +36,15 @@ PLA::PLA(C64 * c64) :
   if (!havecart) { /* default mode 31, all bits high */
     setup_memory_banks(kLORAM|kHIRAM|kCHARGEN|kGAME|kEXROM);
   } else { /* cart depends on cart type */
-    setup_memory_banks(kLORAM|kHIRAM|kCHARGEN/* |kGAME|kEXROM */); /* TODO: CHANGE */
+    if (c64_->cart_->cartactive) {
+      setup_memory_banks(c64_->cart_->banksetup);
+    } else {
+      /* Fallback settings */
+      setup_memory_banks(kLORAM|kHIRAM|kCHARGEN);
+    }
   }
+  /* configure data directional bits to default boot setting */
+  c64_->memory()->write_byte_no_io(Memory::kAddrDataDirection, data_direction_default);
 }
 
 PLA::~PLA()
@@ -54,9 +61,24 @@ PLA::~PLA()
   * kGAME    ~ 4
   * kEXROM   ~ 5
   *
+  * banks_[] variable gets set up with the values
+  * from kBankCfg referenced by they bit location
+  *
   * In mode 31 (default) all bits are high (1)
+  * and setting this mode equals:
+  * kEXROM,kGAME,kCHARGEN,kHIRAM,kLORAM
+  *      1,    1,       1,     1,     1
+  *
   * In mode 20 bits are 10100 this corresponds to:
-  * kEXROM,0,kCHARGEN,0,0
+  * kEXROM,      kCHARGEN
+  *      1,    0,       1,     0,     0
+  *
+  * So banks_[]
+  * [kBankRam0,kBankRam1,kBankCart,kBankBasic,kBankRam2,kBankChargen,kBankKernal]
+  * will look like this for mode 32:
+  * [1,1,1,0,1,2,0]
+  * and for mode 20:
+  * [1,-1,3,-1,-1,2,4]
   */
 void PLA::switch_banks(uint8_t v)
 {
@@ -239,7 +261,7 @@ void PLA::switch_banks(uint8_t v)
 }
 
 /**
- * @brief configure memory banks on boot
+ * @brief configure memory banks on boot and PLA reset
  *
  * There are five latch bits that control the configuration allowing
  * for a total of 32 different memory layouts, for now we only take
@@ -248,51 +270,60 @@ void PLA::switch_banks(uint8_t v)
 void PLA::setup_memory_banks(uint8_t v) // TODO: If havecart enable cartrom etc!
 {
   /* load ROMs */
-  c64_->mem_->load_rom("basic.901226-01.bin",Memory::kBaseAddrBasic);
-  c64_->mem_->load_rom("characters.901225-01.bin",Memory::kBaseAddrChars);
-  c64_->mem_->load_rom("kernal.901227-03.bin",Memory::kBaseAddrKernal);
+  if (!l_basic) { /* Only load if not already loaded */
+    if (c64_->mem_->load_rom("roms/basic.901226-01.bin",Memory::kBaseAddrBasic)) {
+    l_basic = true; /* True for success / loaded */
+    } else { l_basic = false; } /* False for failed / not loaded */
+  }
+  if (!l_chargen) { /* Only load if not already loaded */
+    if (c64_->mem_->load_rom("roms/characters.901225-01.bin",Memory::kBaseAddrChars)) {
+      l_chargen = true; /* True for success / loaded */
+    } else { l_chargen = false; } /* False for failed / not loaded */
+  }
+  if (!l_kernal) { /* Only load if not already loaded */
+    if (c64_->mem_->load_rom("roms/kernal.901227-03.bin",Memory::kBaseAddrKernal)) {
+      l_kernal = true; /* True for success / loaded */
+    } else { l_kernal = false; } /* False for failed / not loaded */
+  }
 
-
-  /* start with mode 1 and set everything to ram */
+  /* start with mode 0 and set everything to ram */
   for(size_t i=0 ; i < sizeof(banks_) ; i++) {
     banks_[i] = kRAM;
   }
-
   /* Switch banks on boot */
-  if(logplabank){D("Bank setup @ boot to: %02X %02X\n",v,(v&0x1f));}
-  switch_banks(v);
-
-  /* write the config to the zero page */
+  banks_at_boot = v;
+  switch_banks(banks_at_boot);
+  if(logplabank) {
+    D("Bank setup @ boot to: %02X\n",banks_at_boot);
+    logbanksetup();
+  }
+  /* write the raw value to the zero page (doesn't influence the cart bits) */
   c64_->memory()->write_byte_no_io(Memory::kAddrMemoryLayout, v);
-
-  /* configure data directional bits */
-  c64_->memory()->write_byte_no_io(Memory::kAddrDataDirection, data_direction_default);
-  /* TODO: Must account for Cartridge and cartridge ROMS aswell! */
 }
 
 /**
  * @brief configure memory banks during runtime, limited to 3 cpu latches
  *
- * During runtime the system can only change bit 0,1 and 2
- *
+ * During runtime the system can only change the cpu latches at bit 0,1 and 2
  */
 void PLA::runtime_bank_switching(uint8_t v) /* TODO: UPDATE TO NEW MODE SWITCHING WAY */
 {
   /* Setup bank mode during runtime */
-  uint8_t c = c64_->mem_->read_byte_no_io(Memory::kAddrMemoryLayout);
-  uint8_t b = (0x18|(v&0x1f)); /* 0x18 is temorary set 11000 for banks, cannot change carts during runtime */
-  if(logplabank){D("[PLA] Bank switch @ runtime from %02X to: %02X %02X\n",c,v,b);};
+
+  uint8_t b = banks_at_boot; /* Use boot time state as preset */
+  b &= (0x18|v&0x7); /* Preserve _cart bits_ and only set cpu latches */
+  if(logplabank){
+    D("[PLA] Bank switch @ runtime from %02X to: %02X\n",banks_at_boot,b);
+    logbanksetup();
+  }
   switch_banks(b);
 
-  /* write the config to the zero page */
-  c64_->mem_->write_byte_no_io(Memory::kAddrMemoryLayout, (c|(v&0x7)));
-
-  /* configure data directional bits */
-  c64_->mem_->write_byte_no_io(Memory::kAddrDataDirection, data_direction_default);
+  /* write the raw value to the zero page (doesn't influence the cart bits) */
+  c64_->mem_->write_byte_no_io(Memory::kAddrMemoryLayout, v);
 }
 
 /* Not working, broken and unfinished */
-void PLA::c1541(void)
+void PLA::c1541(void) /* TODO: Move elsewhere, e.g. iec.cpp? */
 {
   return; /* Disabled */
   disk = new uint8_t[disksize]();
@@ -312,8 +343,8 @@ void PLA::c1541(void)
 }
 
 void PLA::write_pla(uint16_t addr, uint8_t v)
-{ /* Unused */
-
+{
+   /* Unused */
 }
 
 uint8_t PLA::read_pla(uint16_t addr)
@@ -322,12 +353,29 @@ uint8_t PLA::read_pla(uint16_t addr)
   return retval;
 }
 
+/**
+ * @brief When used will reset the bank configuration to default
+ */
 void PLA::reset(void)
-{ /* Unused */
-
+{
+  setup_memory_banks(kLORAM|kHIRAM|kCHARGEN|kGAME|kEXROM);
 }
 
 void PLA::emulate(void)
-{ /* Unused */
+{
+  /* Unused */
+}
 
+/* Debug functions */
+void PLA::logbanksetup(void)
+{
+  D("Rm Rm Ct Bc Rm Cn Kl\n%2X %2X %2X %2X %2X %2X %2X\n",
+    memory_banks(kBankRam0),
+    memory_banks(kBankRam1),
+    memory_banks(kBankCart),
+    memory_banks(kBankBasic),
+    memory_banks(kBankRam2),
+    memory_banks(kBankChargen),
+    memory_banks(kBankKernal)
+  );
 }
--- src/pla.h
+++ src/pla.h
@@ -43,8 +43,14 @@ class PLA
     /* Cart yes or no? */
     bool havecart = false;
 
+    /* Roms loaded? */
+    bool l_basic   = false;
+    bool l_chargen = false;
+    bool l_kernal  = false;
+
     /* Memory banks */
     uint8_t data_direction_default = 0x2F; /* (https://www.c64-wiki.com/wiki/Zeropage) */
+    uint8_t banks_at_boot = 0x1F;
     uint8_t banks_[7];
 
     /* Devices */
@@ -53,35 +59,21 @@ class PLA
     /* Debug logging */
     bool logplabank  = false;
 
-    /* Internal bank switching */
-    void switch_banks(uint8_t v);
-
   public:
     PLA(C64 * c64); /* Setup memory on init */
     ~PLA();
 
-    /* bank switching */
-    enum kBankCfg
-    { /* CPU Control Lines */
-      kROM, /* 0 */
-      kRAM, /* 1 */
-      kIO,  /* 2 */
-      kCLO, /* 3 */
-      kCHI, /* 4 */
-      kUNM = -1, /* Unmapped */
-    };
-
-    /* Bank Switching Zones
+    /** Bank Switching Zones
      *
-     * Hex Address Dec Address Page         Size Contents
-     * $0000-$0FFF 0-4095      Page 0-15       4 kBytes RAM (which the system requires and must appear in each mode)
-     * $1000-$7FFF 4096-32767  Page 16-127    28 kBytes RAM or is unmapped
-     * $8000-$9FFF 32768-40959 Page 128-159    8 kBytes RAM or cartridge ROM
-     * $A000-$BFFF 40960-49151 Page 160-191    8 kBytes RAM, BASIC interpretor ROM, cartridge ROM or is unmapped
-     * $C000-$CFFF 49152-53247 Page 192-207    4 kBytes RAM or is unmapped
-     * $D000-$DFFF 53248-57343 Page 208-223    4 kBytes RAM, Character generator ROM, or I/O registers and Color RAM
-     * $E000-$FFFF 57344-65535 Page 224-255    8 kBytes RAM, KERNAL ROM or cartridge ROM
-     *
+     * @brief as described at https://www.c64-wiki.com/wiki/Bank_Switching#Bank_Switching_Zones
+     * ID Hex Address Dec Address Page         Size Contents
+     *  0 $0000-$0FFF 0-4095      Page 0-15       4 kBytes RAM (which the system requires and must appear in each mode)
+     *  1 $1000-$7FFF 4096-32767  Page 16-127    28 kBytes RAM or is unmapped
+     *  2 $8000-$9FFF 32768-40959 Page 128-159    8 kBytes RAM or cartridge ROM
+     *  3 $A000-$BFFF 40960-49151 Page 160-191    8 kBytes RAM, BASIC interpretor ROM, cartridge ROM or is unmapped
+     *  4 $C000-$CFFF 49152-53247 Page 192-207    4 kBytes RAM or is unmapped
+     *  5 $D000-$DFFF 53248-57343 Page 208-223    4 kBytes RAM, Character generator ROM, or I/O registers and Color RAM
+     *  6 $E000-$FFFF 57344-65535 Page 224-255    8 kBytes RAM, KERNAL ROM or cartridge ROM
      */
     enum Banks
     { kBankRam0    = 0, /* RAM, cannot be changed */
@@ -93,15 +85,30 @@ class PLA
       kBankKernal  = 6, /* RAM, Kernal ROM or cartridge ROM */
     };
 
-    /* bank switching */
-    static const uint8_t kLORAM   = 1 << 0;
-    static const uint8_t kHIRAM   = 1 << 1;
-    static const uint8_t kCHARGEN = 1 << 2;
-    static const uint8_t kGAME    = 1 << 3;
-    static const uint8_t kEXROM   = 1 << 4;
+    /* Bank Switching Bit ID's */
+    enum kBankCfg
+    { /* CPU Control Lines */
+      kROM, /* bit 0, weight  1 */
+      kRAM, /* bit 1, weight  2 */
+      kIO,  /* bit 2, weight  4 */
+      /* Expansion Port Pins 8 and 9 */
+      kCLO, /* bit 3, weight  8 */
+      kCHI, /* bit 4, weight 16 */
+      kUNM = -1, /* Unmapped */
+    };
 
+    /* Bank Switching Bits */
+    static const uint8_t kLORAM   = 1 << kROM; /* 0 */
+    static const uint8_t kHIRAM   = 1 << kRAM; /* 1 */
+    static const uint8_t kCHARGEN = 1 << kIO;  /* 2 */
+    static const uint8_t kGAME    = 1 << kCLO; /* 3 */
+    static const uint8_t kEXROM   = 1 << kCHI; /* 4 */
+
+    /** Bank Switching Modes
+     * @brief as decribed at https://www.c64-wiki.com/wiki/Bank_Switching#Mode_Table
+     */
     enum Modes
-    {
+    { /* Visible = 1, Not visible = 0 */
       m31 = (kEXROM|kGAME|kCHARGEN|kHIRAM|kLORAM),
       m30 = (kEXROM|kGAME|kCHARGEN|kHIRAM),
       m29 = (kEXROM|kGAME|kCHARGEN|       kLORAM),
@@ -136,6 +143,9 @@ class PLA
       m00 = 0,
     };
 
+    /* Internal bank switching, public for Loader::bin */
+    void switch_banks(uint8_t v);
+
     /* Memory banking / glue logic */
     uint8_t memory_banks(int v){return banks_[v];};
     void setup_memory_banks(uint8_t v);
@@ -156,6 +166,7 @@ class PLA
 
     /* Debug logging */
     void setbanklogging(bool v){logplabank = v;};
+    void logbanksetup(void);
 };
 
 
