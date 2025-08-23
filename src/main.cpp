4c4b6c76750c85afed7a0c8ed3374e4a9ace087e
loudnl <LouDnl@users.noreply.github.com>
2025-08-23 15:46:36 +0200

    Move argument handling from loader to main

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,10 @@
 C64 *c64;
 Loader *loader;
 bool wget_download_finished = false;
+bool file_loaded = false;
 bool nosdl = false, isbinary = false,
-     havecart = false, bankswlog = false;
+     havecart = false,
+     midi = false, bankswlog = false;
 
 
 bool loader_cb()
@@ -40,7 +42,7 @@ bool loader_cb()
   return true;
 }
 
-void load_file(const char *file) /* TODO: ADD ARGUMENTS FOR SIDFILE PLAY */
+bool load_file(const char *file) /* TODO: ADD ARGUMENTS FOR SIDFILE PLAY */
 {
   // const char *file = argv[1];
   std::string f(file);
@@ -62,43 +64,73 @@ void load_file(const char *file) /* TODO: ADD ARGUMENTS FOR SIDFILE PLAY */
       loader->d64(f);
     }
     else if(ext == "crt"){
-      loader->crt(f);
+      havecart = loader->crt();
+      return false; /* Circumvent callback for cart */
     }
     else if(ext == "sid"){
       loader->sid(f);
     }
   }
+  return true;
 }
 
 void checkargs(int argc, char **argv)
 {
   if(argc>=1) {
     for(int a = 1; a < argc; a++) {
-      if(!strcmp(argv[a], "-cli")) {nosdl = true;};
-      if(!strcmp(argv[a], "-bin")) {isbinary = true;};
-      if(!strcmp(argv[a], "-crt")) {havecart = true;};
-
       /* log on create */
-      if(!strcmp(argv[a], "-logbanksw")) bankswlog = true;
+      if(!strcmp(argv[a], "-cli")) {nosdl = true;}
+      if(!strcmp(argv[a], "-bin")) {isbinary = true;}
+      if(!strcmp(argv[a], "-midi")) {midi = true;} /* BUG: Segmentation fault when used with loading a .bin file */
+      if(!strcmp(argv[a], "-logbanksw")) {bankswlog = true;}
+
+      if(!strcmp(argv[a], "-crt")) {loader->iscart = true;}
+      if(!strcmp(argv[a], "-init")) {loader->init_addr = strtol(argv[a+1], NULL, 16);}
+      if(!strcmp(argv[a], "-norun")) {loader->autorun = false;}
+      if(!strcmp(argv[a], "-lowercase")) {loader->lowercase = true;}
+      if(!strcmp(argv[a], "-loginstr")) {Cpu::loginstructions = true;}
+      if(!strcmp(argv[a], "-logill")) {Cpu::logillegals = true;}
+      if(!strcmp(argv[a], "-logmemrw")) {loader->memrwlog = true;}
+      if(!strcmp(argv[a], "-logcia1rw")) {loader->cia1rwlog = true;}
+      if(!strcmp(argv[a], "-logcia2rw")) {loader->cia2rwlog = true;}
+      if(!strcmp(argv[a], "-logsidrw")) {loader->sidrwlog = true;}
+      if(!strcmp(argv[a], "-logiorw")) {loader->iorwlog = true;}
+      if(!strcmp(argv[a], "-logplarw")) {loader->plarwlog = true;}
+      if(!strcmp(argv[a], "-logcartrw")) {loader->cartrwlog = true;}
+
+      /* Check for file */
+      if((strchr(argv[a], '.') != NULL)) {loader->filename = argv[a];}
+
     }
   }
 }
 
 int main(int argc, char **argv)
 {
-  checkargs(argc, argv);
-  c64 = new C64(nosdl,isbinary,havecart,bankswlog);
-  /* check if asked load a program */
-  if(argc != 1)
-  {
-    loader = new Loader(c64);
-    loader->process_args(argc,argv);
-    c64->callback(loader_cb);
-    if (loader->file != NULL) {
-      load_file(loader->file);
+  loader = new Loader();
+  if(argc != 1) {
+    checkargs(argc, argv);
+    if (loader->filename != NULL) {
+      file_loaded = load_file(loader->filename);
     }
-    // loader->handle_args();
   }
+
+  /* Init Machine start */
+  c64 = new C64(nosdl,isbinary,havecart,bankswlog,midi,loader->filename);
+
+  if (argc != 1) {
+    loader->C64ctr(c64); /* Init machine in Loader */
+    /* check if asked load a program */
+    if(file_loaded) {
+      /* Process arguments and handle file loading callback */
+      c64->callback(loader_cb);
+    } else {
+      /* Only process arguments */
+      loader->handle_args();
+    }
+  }
+
+  /* Continue machine startup */
   c64->start();
   return 0;
 }
